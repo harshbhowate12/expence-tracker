0281cd027f7b6da6c17cc792997756454c401c49
harshbhowate12 <harshitbhowate11@gmail.com>
2024-08-13 20:24:11 +0530

    added models

---
+++ server/models/User.js
@@ -0,0 +1,27 @@
+import { Schema,model } from "mongoose";
+
+const userSchema=new Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    DOB:{
+        type:Date,
+        required:true
+    }
+},{
+    timestamps:true
+})
+
+const User=model("User",userSchema);
+
+export default User;
